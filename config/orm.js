c8f3c19b7bf38f87c4c063e7bfd68f259d16a407
Eva Anderson <eva.k.anderson@gmail.com>
2019-05-01 10:35:32 -0500

    got adding working

--- config/orm.js
+++ config/orm.js
@@ -36,23 +36,6 @@ var orm = {
       cb(result);
     });//  **** -----End create() fct ORM -----***
   },
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
 
 
   // var orm = {
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,25 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+  // $(".change-devour").on("click", function(event) {
+  //   var id = $(this).data("id");
+  //   var newDevour= $(this).data("newdevour");
+
+  //   var newDevourState = {
+  //     devoured: newDevour
+  //   };
+
+  //   // Send the PUT request.
+  //   $.ajax("/api/burgers/" + id, {
+  //     type: "PUT",
+  //     data: newDevourState
+  //   }).then(
+  //     function() {
+  //       console.log("changed devour to", newDevour);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
@@ -18,28 +38,6 @@ $(function() {
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".change-burger").on("click", function(event) {
-    console.log ("registering onclick")
-    var id = $(this).data("id");
-    var newDevour= false;
-
-    var newDevourState = {
-      devoured: newDevour
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevourState
-    }).then(
-      function() {
-        console.log("changed devour to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
